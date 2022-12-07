0a3912d3071fee1745104270f58ab462903b17ce
Titan0932 <anjishpradhananga@outlook.com>
2022-12-06 22:11:52 -0500

    Documented pageRankData.java -- ANJ

--- src/projectClasses/PagerankData.java
+++ src/projectClasses/PagerankData.java
@@ -6,6 +6,11 @@ import java.io.FileReader;
 import java.text.DecimalFormat;
 
 public class PagerankData implements SearchResult {
+    /*
+     * Implements the SearchResult interface
+     * Stores the score calculated from search query and the title for a url
+     */
+
     private String url;
     // private String ORDER;  //descending or ascending order
     private Double score;
@@ -38,7 +43,7 @@ It calculates the score by multiplying the score of each url by its pagerank val
 :param cosine_similarity: dictionary of the cosine similarity of every url.
 */
 public void setScore(Double pagerank){
-    DecimalFormat df = new DecimalFormat("#.###");
+    DecimalFormat df = new DecimalFormat("#.###");  //rounding to 3 decimal pts
     this.score= Double.valueOf(df.format(this.score * pagerank));
     // this.score= Math.round(this.score * pagerank *1000d)/1000d;
 }
