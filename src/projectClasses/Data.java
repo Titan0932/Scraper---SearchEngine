5c9f3c98493daac40fe6ca0f134af6a662e145e6
Titan0932 <anjishpradhananga@outlook.com>
2022-12-06 22:05:35 -0500

    documented Data.java-- ANJ

--- src/projectClasses/Data.java
+++ src/projectClasses/Data.java
@@ -3,7 +3,8 @@ package projectClasses;
 import java.util.List;
 
 record Data(List<String> data){
-
+    // This is an immutable record structure that stores all parsed data. It makes sure that your parsed data doesn't get changed by accident while performing analysis. Also, if there's many elements of data 
+    // parsed then, this makes it easier to understand the data and use them as well.
 
     public String getTitle(){
         return data.get(0);
