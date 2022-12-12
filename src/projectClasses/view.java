c3d3bd3a8314b9bd523f58ad16cda6a2573c5c9a
Iman1121 <imanullah1112@gmail.com>
2022-12-12 11:57:12 -0500

    Commented and improved on GUI

--- src/projectClasses/view.java
+++ src/projectClasses/view.java
@@ -1,21 +1,12 @@
 package projectClasses;
 
 import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
-
-
-
-
-
-import java.awt.*;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-
 import javafx.scene.control.ToggleButton;
 
 public class view extends Pane{
@@ -24,51 +15,50 @@ public class view extends Pane{
     private Button searchButton;
     private ToggleButton pageRank;
     private Crawler crawl;
-    private ListView<Integer> number;
-    private ListView<Double> scores;
     private Label label1,label2,label3, label4, error;
     public view(){
         crawl = new Crawler();
+        //takes user searchquery input
         label1 = new Label("Search");
         label1.relocate(40,30);
         search = new TextField();
         search.setPrefSize(255,10);
         search.relocate(140,25);
-
+        //asks user how many search result they want
         label4 = new Label("Amount of results");
         label4.relocate(40,60);
         amountResults = new TextField();
         amountResults.setPrefSize(130,10);
         amountResults.relocate(140,55);
 
-        label2 = new Label("Result");
-        label2.relocate(140,90);
-        rList = new ListView<String>();
-        rList.setPrefSize(115,235);
-        rList.relocate(140,110);
+        //button for searching
         searchButton = new Button("Search");
         searchButton.setStyle("-fx-font: 12 arial; -fx-base: rgb(0,100,0); -fx-text-fill: rgb(255,255,255);");
         searchButton.relocate(280,55);
+
+        //button for activating pagerank
         pageRank = new ToggleButton("Boost");
         pageRank.relocate(345,55);
         pageRank.setStyle("-fx-font: 12 arial;");
-
-        number = new ListView<Integer>();
-
-        number.setPrefSize(45, 235);
-        number.relocate(80,110);
-
         label3 = new Label("Scores");
-        label3.relocate(275,90);
-        scores = new ListView<Double>();
-        scores.setPrefSize(90,235);
-        scores.relocate(275,110);
+        label3.relocate(235,90);
 
+        //attributes for displaying output
+        label2 = new Label("Result");
+        label2.relocate(110,90);
+        rList = new ListView<String>();
+        rList.setPrefSize(275,235);
+        rList.relocate(80,110);
+
+        //an error label for when they don't provide proper input in amount of results
         error = new Label();
         error.relocate(90,5);
-        getChildren().addAll(label1,label2,search, rList,searchButton, pageRank, number, scores,label3, amountResults,label4, error);
+
+
+        getChildren().addAll(label1,label2,search, rList,searchButton, pageRank, amountResults,label4, error, label3);
         setPrefSize(435,375);
     }
+    //getters for proper event handling on GUI class
     public TextField getSearch(){
         return search;
     }
@@ -83,33 +73,24 @@ public class view extends Pane{
     public ListView<String> getResultList(){
         return rList;
     }
-    public ListView<Integer> getNumber(){
-        return number;
-    }
-    public ListView<Double> getScores(){
-        return  scores;
-    }
+
+    //Implements search, runs search method after the user clicks the search button
     public void update(int i){
-        try{
+        try{//try catch block for catching invalid input
             error.setVisible(false);
             ArrayList<String> result = new ArrayList<>();
             Integer amount = Integer.parseInt(amountResults.getText());
-            Integer[] numbers = new Integer[amount];
-            ArrayList<Double> score= new ArrayList<>();
-            for(int j = 0; j<amount; j++){
-                numbers[j] = j+1;
-
-            }
-
+            int count = 0;
             for(SearchResult x: crawl.search(search.getText(), pageRank.isSelected(), amount)){
-                result.add(x.getTitle());
-                score.add(x.getScore());
+                count ++;
+                String s = String.format("%4d.  %-35s%-5.3f",count,x.getTitle(), x.getScore());
+                System.out.println(s);
+                result.add(s);
+
             }
             rList.setItems(FXCollections.observableArrayList(result));
-            number.setItems(FXCollections.observableArrayList(numbers));
-            scores.setItems(FXCollections.observableArrayList(score));
-            number.getSelectionModel().select(i);
-            scores.getSelectionModel().select(i);
+
+
             rList.getSelectionModel().select(i);
         }catch(NumberFormatException e){
             error.setText("Please provide an Integer for Amount of Results");
@@ -117,15 +98,8 @@ public class view extends Pane{
         }
 
     }
-    public void update2(int i){
-        number.getSelectionModel().select(i);
-        scores.getSelectionModel().select(i);
-        rList.getSelectionModel().select(i);
-        number.scrollTo(i);
-        scores.scrollTo(i);
-        rList.scrollTo(i);
-    }
-    public void updateToggle(){
+
+    public void updateToggle(){//to change color of pagerank button when selected, should be blue when selected
         if(pageRank.isSelected()){
             pageRank.setStyle("-fx-base: white");
         }
